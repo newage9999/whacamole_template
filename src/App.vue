1faa7457b8f368fa40d25b6c05425493f2ef58a5
newage9999 <roger.solis@uab.cat>
2024-04-16 21:38:17 +0200

    Project created through 'npm create vue@latest'

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div style="display: flex; flex-wrap: wrap">
+    <Mole></Mole><Mole></Mole><Mole></Mole>
+    <Mole></Mole><Mole></Mole><Mole></Mole>
+  </div>
+</template>
+
+<script>
+import Mole from './components/Mole.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Mole
+  }
+};
+</script>
